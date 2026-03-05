6c8d85b406175ca15603c7bbdcfe6e74c0cd5953
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-05 19:29:36 -0300

    Initial Commit

---
+++ QuickConvert/QuickConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickConvertApp.swift
+//  QuickConvert
+//
+//  Created by Brayan Duwe on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
